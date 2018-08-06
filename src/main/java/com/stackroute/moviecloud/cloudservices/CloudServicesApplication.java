f25d2f7714d6aaad8b2605d73e2a78191d46b86e
amarnath <t.amarnath@hotmail.com>
2018-08-06 14:10:19 +0530

    layout

--- src/main/java/com/stackroute/moviecloud/cloudservices/CloudServicesApplication.java
+++ src/main/java/com/stackroute/moviecloud/cloudservices/CloudServicesApplication.java
@@ -1,4 +1,4 @@
-package com.stackroute.moviecloud.CloudServices;
+package com.stackroute.moviecloud.cloudservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
